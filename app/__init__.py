449418b0b73be48a8bb41c21a0805ce4e5ae391d
Peter Polle <peter.m.polle@gmail.com>
2018-04-17 11:46:58 +0300

    updated email configurations

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -11,6 +12,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -22,6 +24,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
 
     # configure UploadSet
     configure_uploads(app,photos)
--- config.py
+++ config.py
@@ -5,6 +5,12 @@ class Config:
 	UPLOADED_PHOTOS_DEST ='app/static/photos'
 	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/database_name'
 
+	MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 	@staticmethod
 	def init_app(app):
 		pass
