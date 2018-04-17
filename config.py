90929803cf37560bb8cf2b9f823814b6e0b1e18f
Peter Polle <peter.m.polle@gmail.com>
2018-04-17 12:14:06 +0300

    added README.md

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class Config:
 	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/database_name'
 
 	MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 	@staticmethod
 	def init_app(app):
 		pass
